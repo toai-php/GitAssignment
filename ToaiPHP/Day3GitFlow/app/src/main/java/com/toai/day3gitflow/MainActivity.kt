72e3560f8e365c2bdf002f71d22e19bcdd5dfb3e
toai-php <56766134+toai-php@users.noreply.github.com>
2022-01-13 01:11:21 +0700

    add fun Fibonacci

---
+++ ToaiPHP/Day3GitFlow/app/src/main/java/com/toai/day3gitflow/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.toai.day3gitflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun Fibonacci(n : Int, flag: Boolean) {
+
+    }
+}
